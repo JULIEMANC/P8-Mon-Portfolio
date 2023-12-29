0ecab4ec54d5a5cbe9ea1e9a8743f83ef723cbb8
JULIEMANC <136808377+JULIEMANC@users.noreply.github.com>
2023-12-29 16:13:09 +0100

    Amélioration du Sass

--- src/componements/about.jsx
+++ src/componements/about.jsx
@@ -1,42 +1,47 @@
 import React from "react";
 import fixture from "../data.json";
-// import ButtonContact from '../componement/Contact';
 
 const About = () => {
   return (
     <>
       <section className="propos" id="about">
-        <h1 className="nom">MANCINO JULIE</h1>
-        <h2 className="metier"> DEVELOPPEUR WEB FRONT-END</h2>
-        <p>Façonnons ensemble votre vision numérique.</p>
-        <div className="buttons" >
-          <a className="contact1" href="/Contact" target="_blank">
-            <button className="contact">Contactez-moi</button>
-          </a>
-          <a
-            className="github"
-            href="https://github.com/dashboard"
-            target="_blank"
-            rel="noreferrer">
-            <button className="git">Github</button>
-          </a>
+        <div className="proposBlock">
+          <h1 className="nom">MANCINO JULIE</h1>
+          <h2 className="metier"> DEVELOPPEUR WEB FRONT-END</h2>
+          <p>Façonnons ensemble votre vision numérique.</p>
+          <div className="button">
+            <a href="#contact">
+              {/* <a className="contact1" href="/Contact" target="_blank"> */}
+              <button className="contact">Contactez-moi</button>
+            </a>
+            <a
+              className="github"
+              href="https://github.com/dashboard"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button className="git">Github</button>
+            </a>
+          </div>
         </div>
-
-        {/* <img
-          className="PhotoMoi"
-          src="../assets/images/moi.jpeg"
-          alt="moi de près "
-        /> */}
+        <div className="photoMe">
+          <img
+            className="PhotoMoi"
+            src="../assets/images/moi.jpeg"
+            alt="moi de près "
+          />
+        </div>
       </section>
-
       <section className="block2">
-        {/* <img
-          className="PhotoCode"
-          src="../assets/images/code.webp"
-          alt="code React vu de près"
-        /> */}
-        <h2 className="title1">A propos</h2>
-        <p className="texte1">{fixture.about.description}</p>
+        <h2 className="title1">À propos de moi :</h2>
+        <div className="block">
+          <img
+            className="PhotoCode"
+            src="../assets/images/code.webp"
+            alt="code React vu de près"
+          />
+          <p className="texte1">{fixture.about.description}</p>
+        </div>
       </section>
     </>
   );
